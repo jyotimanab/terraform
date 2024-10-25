d6f4d2abf0a9ad7744e1507be015d214ad7e2039
jyotimanab <43140170+jyotimanab@users.noreply.github.com>
2024-10-25 07:59:33 +0000

    terraform variable applied

---
+++ day1/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_value = "ami-0866a3c8686eaeeba"
+instance_type_value = "t2.micro"
---
+++ day1/variables.tf
@@ -0,0 +1,6 @@
+variable ami_value {
+  description = "ami value"
+}
+variable instance_type_value {
+  description = "instance type value"
+}
